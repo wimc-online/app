aaf7b629ef8d1db4492be028a381d4b8a0444021
Maciej Szklarczyk <maciej.szklarczyk@protonmail.com>
2020-07-26 21:18:40 +0200

    Courier view switch

--- src/components/CourierComponents.tsx
+++ src/components/CourierComponents.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import Geolocation from "./Geolocation";
+import ApiCall from "./ApiCall";
+import keycloak from "../keycloak";
+import GetTasks from "./orders/GetTasks";
+import AddTask from "./orders/AddTask";
+import {KeycloakInstance} from "keycloak-js";
 
+interface ContainerProps {
+    page: string
+}
 
-const CourierComponents: React.FC = () => {
+function renderSwitch({page}: { page: string }) {
+    switch (page) {
+        case 'Dashboard':
+            return (
+                <div>
+                    <Geolocation />
+                </div>
+            );
+        case 'Orders':
+            return (
+                <div>
+                    <GetTasks keycloak={keycloak}/>
+                </div>
+            );
+        default:
+            return 'default';
+    }
+}
+
+const CourierComponents: React.FC<ContainerProps> = ({page}) => {
     return (
         <div>
-            <Geolocation />
+            {renderSwitch({page: page})}
         </div>
     )
 };
